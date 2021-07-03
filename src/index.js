c8b6b70550095bea15c82cfc0f1d3813dc886fe8
Aline Furquim <aline1080@gmail.com>
2021-07-03 19:08:01 -0300

    :art:

--- src/index.js
+++ src/index.js
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
- 
+ //setup var
+
+const fisrtBook = {
+  img: "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/mp_941f4c8a279ad61206347ab715257244.jpg",
+  title: 'O perigo de uma história única',
+  author: 'Chimamanda Ngozi Adichie',
+};
+
+const secondBook = {
+  img: "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/d43ecf3b8188e024e1b7611a4f1d371451aeac85.jpg",
+  title: 'Americanah',
+  author: 'Chimamanda Ngozi Adichie',
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book 
+        img={fisrtBook.img} 
+        title={fisrtBook.title} 
+        author={fisrtBook.author} 
+        >
+        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+       </p>
+      </Book>
+      
+      <Book 
+        img={secondBook.img} 
+        title={secondBook.title} 
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const author = 'The College Board';
-const Book = () => {
-  const title = 'Official SAT Study Guide 2020 Edition';
+const Book = (props) => {
+  const {img, title, author, Children} = props;
   return (
     <article className="book">
       <img 
-        src="https://images-na.ssl-images-amazon.com/images/I/41wBo8-XN7L._AC_SX368_.jpg" 
+        src={img}
         alt=' '/>
       <h1>{title}</h1>
-      <h4>{author}</h4>     
+      <h4>{author}</h4>
+      {Children}
     </article>
   );
 };
