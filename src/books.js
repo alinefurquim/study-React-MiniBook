5564b7645a4a08fc5a6a8335172a18b9dcfa1745
Aline Furquim <aline1080@gmail.com>
2021-07-04 09:41:49 -0300

    Inserting events basics, key prop and spread operator

---
+++ src/books.js
@@ -0,0 +1,23 @@
+export const books = [
+
+    {
+      id: 1,
+      img: "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/mp_941f4c8a279ad61206347ab715257244.jpg",
+      title: 'O perigo de uma história única',
+      author: 'Chimamanda Ngozi Adichie',
+    },
+  
+    {
+      id: 2,
+      img: "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/d43ecf3b8188e024e1b7611a4f1d371451aeac85.jpg",
+      title: 'Americanah',
+      author: 'Chimamanda Ngozi Adichie',
+    },
+  
+    {
+      id: 3,
+      img: "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/_886e659dc4a7bec7d6db01a5305a00f6d9002451.jpg",
+      title: 'Para educar crianças feministas',
+      author: 'Chimamanda Ngozi Adichie',
+    },
+  ];
--- src/index.js
+++ src/index.js
@@ -1,53 +1,20 @@
 import React, { Children } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
- //setup var
-
-const fisrtBook = {
-  img: "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/mp_941f4c8a279ad61206347ab715257244.jpg",
-  title: 'O perigo de uma história única',
-  author: 'Chimamanda Ngozi Adichie',
-};
-
-const secondBook = {
-  img: "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/d43ecf3b8188e024e1b7611a4f1d371451aeac85.jpg",
-  title: 'Americanah',
-  author: 'Chimamanda Ngozi Adichie',
-};
+import {books} from './books'
+import Book from './Book'
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book 
-        img={fisrtBook.img} 
-        title={fisrtBook.title} 
-        author={fisrtBook.author} 
-        >
-        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-       </p>
-      </Book>
-      
-      <Book 
-        img={secondBook.img} 
-        title={secondBook.title} 
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const {img, title, author, Children} = props;
-  return (
-    <article className="book">
-      <img 
-        src={img}
-        alt=' '/>
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      {Children}
-    </article>
-  );
-};
+
 
 ReactDOM.render(<BookList />,document.getElementById('root'));
